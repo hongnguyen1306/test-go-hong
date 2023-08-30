50ea1b33dc3c633adcaaac8dd5bc5ca9249f7f9c
Hong <hong@Hongs-iMac.local>
2023-08-30 14:32:42 +0700

    connect mysql docker

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
@@ -21,4 +24,13 @@ func main() {
 	})
 	fmt.Println("start service with port: ", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+
+	db, err := sql.Open("mysql", "root:123@tcp(mysql:3306)/TestDB")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	fmt.Println("Table created successfully!", db)
+
 }
