0b89ea9775b46330ea008b930e69c091010ba148
Hong <hong@Hongs-iMac.local>
2023-08-30 16:56:29 +0700

    mutex

--- mutex/mutex.go
+++ mutex/mutex.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var x int64 = 0
+var mutex = &sync.Mutex{}
+
+func addOne(wg *sync.WaitGroup, i int) {
+	fmt.Printf("into goroutine %d\n", i)
+	// Lock lại
+	mutex.Lock()
+	x = x + 1
+	// Unlock
+	mutex.Unlock()
+
+	wg.Done()
+}
+func main() {
+	var w sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		w.Add(1)
+		fmt.Printf("goroutine %d\n", i)
+		go addOne(&w, i)
+	}
+	w.Wait()
+	fmt.Println("Giá trị của x là: ", x)
+}
